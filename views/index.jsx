739e6be3537ea25416afecd1d74f6971d7ebbd95
bryan buitrago <bryanbuitrago86@gmail.com>
2017-08-09 20:42:40 -0400

    first react component

---
+++ views/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class IndexComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React Activated!!</h1>
+      </div>
+    );
+  }
+}
+export default IndexComponent;
